f489a28a99f79a7ada9ac1093e2d076fb6e1a577
Liviu <liviu.preda@t-systems.com>
2024-06-03 11:22:39 +0200

    add 0 health bars, start new game and surrender functionality

--- app.js
+++ app.js
@@ -35,9 +35,15 @@ const app = Vue.createApp({
     },
     computed: {
         monsterBarStyles() {
+            if (this.monsterHealth < 0) {
+                return { width: '0%' };
+            }
             return { width: this.monsterHealth + '%' };
         },
         playerBarStyles() {
+            if (this.playerHealth < 0) {
+                return { width: '0%' };
+            }
             return { width: this.playerHealth + '%' };
         },
         mayUseSpecialAttack() {
@@ -45,6 +51,12 @@ const app = Vue.createApp({
         }
     },
     methods: {
+        startNewGame() {
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.currentRound = 0;
+            this.winner = null;
+        },
         attackMonster() {
             this.currentRound++;
             const attackValue = getRandomValue(5, 12);
@@ -71,6 +83,9 @@ const app = Vue.createApp({
                 this.playerHealth += healValue;
             }
             this.attackPlayer();
+        },
+        surrenderGame() {
+            this.winner = 'monster';
         }
     }
 });
