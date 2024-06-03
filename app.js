8e0fd347d75cd54982b5daea28729b21ff46fe95
Liviu <liviu.preda@t-systems.com>
2024-06-03 10:16:57 +0200

    add special attack functionality

--- app.js
+++ app.js
@@ -1,32 +1,45 @@
 // function for calculating random attack values
 function getRandomValue(min, max) {
-    return Math.floor(Math.random() * (max -min)) + min;
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
 const app = Vue.createApp({
     data() {
-       return {
-        playerHealth: 100,
-        monsterHealth: 100
-       }; 
+        return {
+            playerHealth: 100,
+            monsterHealth: 100,
+            // special attack can only be used every 3 rounds
+            currentRound: 0
+        };
     },
     computed: {
         monsterBarStyles() {
-            return {width: this.monsterHealth + '%'};
+            return { width: this.monsterHealth + '%' };
         },
         playerBarStyles() {
-            return {width: this.playerHealth + '%'};
+            return { width: this.playerHealth + '%' };
+        },
+        mayUseSpecialAttack() {
+            return this.currentRound % 3 !== 0;
         }
     },
     methods: {
         attackMonster() {
-           const attackValue = getRandomValue(5, 12);
-           this.monsterHealth -= attackValue;
-           this.attackPlayer();
+            this.currentRound++;
+            const attackValue = getRandomValue(5, 12);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
         },
         attackPlayer() {
-           const attackValue = getRandomValue(8, 15);
-           this.playerHealth -= attackValue;
+            const attackValue = getRandomValue(8, 15);
+            this.playerHealth -= attackValue;
+        },
+        specialAttackMonster() {
+            this.currentRound++;
+
+            const attackValue = getRandomValue(10, 25);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
         }
     }
 });
